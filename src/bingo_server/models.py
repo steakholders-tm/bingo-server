7739e13e61ac0ad9be1687d6e07b330c28154644
sklirg <hlsolbjorg@gmail.com>
2016-04-03 16:15:31 +0200

    Fix #2

--- src/bingo_server/models.py
+++ src/bingo_server/models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import datetime
 from django.db.models import DateField, TimeField, CharField, Model, ManyToManyField, IntegerField, ForeignKey, DateTimeField
 
 
@@ -14,25 +15,44 @@ class Game(Model):
     primary_category = ForeignKey(to="PrimaryCategory", related_name="games")
     secondary_category = ForeignKey(null=True, blank=True, to="SecondaryCategory", related_name="games")
 
+    def __str__(self):
+        return '"%s" started on %s' % (self.name, self.datetime.strftime("%Y-%m-%d %H:%M:%S"))
+
+    @property
+    def datetime(self):
+        return datetime.datetime.combine(self.date, self.time)
+
 
 class GameType(Model):
     name = CharField(max_length=256)
     description = CharField(blank=True, max_length=512)
+
+    def __str__(self):
+        return self.name
 
 
 class Place(Model):
     name = CharField(max_length=256)
     description = CharField(blank=True, max_length=512)
 
+    def __str__(self):
+        return self.name
+
 
 class PrimaryCategory(Model):
     name = CharField(max_length=256)
     description = CharField(blank=True, max_length=512)
 
+    def __str__(self):
+        return self.name
+
 
 class SecondaryCategory(Model):
     name = CharField(max_length=256)
     description = CharField(blank=True, max_length=512)
+
+    def __str__(self):
+        return self.name
 
 
 class Tile(Model):
@@ -42,12 +62,14 @@ class Tile(Model):
     primary_categories = ManyToManyField(blank=True, to="PrimaryCategory", related_name="tiles")
     secondary_categories = ManyToManyField(blank=True, to="SecondaryCategory", related_name="tiles")
 
+    def __str__(self):
+        return self.name
+
 
 class Winner(Model):
     name = CharField(max_length=256)
     game = ForeignKey(to="Game", related_name="winners")
     time = DateTimeField()
 
-
-
-
+    def __str__(self):
+        return '"%s" won "%s"' % (self.name, self.game.name)

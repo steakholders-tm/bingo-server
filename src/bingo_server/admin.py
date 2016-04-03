b8e90b0ff22dca4249da9edc5c5bf0445480f13e
Dag Frode Solberg <tncditto@gmail.com>
2016-04-03 14:16:05 +0200

    Added Models

--- src/bingo_server/admin.py
+++ src/bingo_server/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Game, Place, Winner, Tile, PrimaryCategory, SecondaryCategory, GameType
 
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Place)
+admin.site.register(Winner)
+admin.site.register(Tile)
+admin.site.register(PrimaryCategory)
+admin.site.register(SecondaryCategory)
+admin.site.register(GameType)

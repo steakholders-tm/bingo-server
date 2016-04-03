d46e57c7c881e62967ed27d1dfbe99c002e1c32a
Dag Frode Solberg <tncditto@gmail.com>
2016-04-03 15:31:18 +0200

    Added PrimaryCategory

--- src/api/urls.py
+++ src/api/urls.py
@@ -6,5 +6,6 @@ from bingo_server.api import views as bingo_server_views
 router = SimpleRouter()
 
 router.register('games', bingo_server_views.GameViewSet)
+router.register('primary-categories', bingo_server_views.PrimaryCategoryViewSet)
 
 urlpatterns = router.urls
--- src/bingo_server/api/views.py
+++ src/bingo_server/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 
-from ..models import Game
-from .serializers import GameSerializer
+from ..models import Game, PrimaryCategory
+from .serializers import GameSerializer, PrimaryCategorySerializer
 
 
 class GameViewSet(ModelViewSet):
     serializer_class = GameSerializer
     queryset = Game.objects.all()
+    permission_classes = (AllowAny,)
+
+
+class PrimaryCategoryViewSet(ModelViewSet):
+    serializer_class = PrimaryCategorySerializer
+    queryset = PrimaryCategory.objects.all()
     permission_classes = (AllowAny,)

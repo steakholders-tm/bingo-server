093f8eefb8864ad5a361952082df8f91fc829f20
sklirg <hlsolbjorg@gmail.com>
2016-04-14 21:07:57 +0200

    Use create-method from superclass

--- src/bingo_server/api/serializers.py
+++ src/bingo_server/api/serializers.py
@@ -57,29 +57,9 @@ class GameSerializer(ModelSerializer):
         )
 
     def create(self, validated_data):
-
-        ModelClass = self.Meta.model
+        instance = super().create(validated_data)
 
         tiles = self.get_tiles(validated_data)
-
-        try:
-            instance = ModelClass.objects.create(**validated_data)
-        except TypeError as exc:
-            msg = (
-                'Got a `TypeError` when calling `%s.objects.create()`. '
-                'This may be because you have a writable field on the '
-                'serializer class that is not a valid argument to '
-                '`%s.objects.create()`. You may need to make the field '
-                'read-only, or override the %s.create() method to handle '
-                'this correctly.\nOriginal exception text was: %s.' %
-                (
-                    ModelClass.__name__,
-                    ModelClass.__name__,
-                    self.__class__.__name__,
-                    exc
-                )
-            )
-            raise TypeError(msg)
 
         instance.tiles.add(*tiles)
         return instance
